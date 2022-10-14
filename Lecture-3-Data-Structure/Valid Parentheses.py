c103809b3ad50095672a6a6549db000903034767
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-10-14 18:09:07 +0300

    Create Valid Parentheses.py

---
+++ Lecture-3-Data-Structure/Valid Parentheses.py
@@ -0,0 +1,18 @@
+ """
+ 
+ first shoot
+
+
+ class Solution(object):
+    def isValid(self, s):
+       
+        :type s: str
+        :rtype: bool
+        
+        for i in range(len(s)):
+          if i+len(nums)=="()" or "{}" or "[]":
+            return bool(True)
+      
+          else:
+            return bool(False)
+      """ 
