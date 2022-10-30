f3e72374b9e0dc985a1c67a7042cb93737a267b1
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-10-30 13:37:05 +0300

    Update 128. Longest Consecutive Sequence.py

--- Lecture-3-Data-Structure/128. Longest Consecutive Sequence.py
+++ Lecture-3-Data-Structure/128. Longest Consecutive Sequence.py
@@ -62,3 +62,34 @@ def longestConsecutive(nums):
 
 nums = [0,3,7,2,5,8,4,6,0,1]
 print(longestConsecutive(nums))
+
+
+
+def longestConsecutive(nums):
+  arr = []
+  j=0
+  nums.sort()
+  for i in range(0,len(nums)):
+      
+
+      if nums[i] == nums[i]+1:
+          nums.pop(i)
+         for i in nums:
+           if nums[i] in range(0,nums[-1]):
+              if nums[i]+1 in nums:
+                  arr.append(i)
+              
+              
+      
+  return len(arr)   
+  
+      
+# Python3 program to find longest
+# contiguous subsequence
+
+# Returns length of the longest
+# contiguous subsequence
+
+
+nums = [0,13,17,12,15,18,14,16,10,11]
+print(longestConsecutive(nums))
